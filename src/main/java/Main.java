79cb92740d5f0d707c8128206d5766700b50e9b4
bjosan <bjosan13@student.westerdals.no>
2015-09-17 11:08:08 +0200

    creating main class

---
+++ src/main/java/Main.java
@@ -0,0 +1,6 @@
+/**
+ * Created by sander on 17.09.15.
+ */
+public class Main {
+
+}
---
+++ src/test/java/MainTest.java
@@ -0,0 +1,8 @@
+import static org.junit.Assert.*;
+
+/**
+ * Created by sander on 17.09.15.
+ */
+public class MainTest {
+
+}
